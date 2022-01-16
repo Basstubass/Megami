11426795101ff9369818bf15d83033a4d928cf4b
Basstubass <0130tubasa@gmail.com>
2022-01-16 22:14:44 +0900

    Hello

--- Megami_project/Megami_app/static/JS/index.js
+++ Megami_project/Megami_app/static/JS/index.js
@@ -1,3 +1,14 @@
-document.getElementById("button").onclick = function() {
-    console.log('Hello')
-  };
+const conceptElement = document.querySelector(".concept");
+const AbilityElement = document.querySelector(".Ability");
+
+window.addEventListener("load", function() {
+    // 実行したい処理
+    conceptElement.classList.add("view")
+ });
+
+ document.addEventListener("scroll", function(){
+    const getElementDistance = AbilityElement.getBoundingClientRect().top + AbilityElement.clientHeight * .6;
+    if (window.innerHeight > getElementDistance) {
+        AbilityElement.classList.add("view")
+    } 
+})
--- Megami_project/Megami_app/urls.py
+++ Megami_project/Megami_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, HomeView
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('home/', HomeView.as_view()),
+
 ]
